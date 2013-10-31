f29b54b8bfd8c46a205cb2f3d03cb2b3e784e2f6
Christian Jülg <gbi-tutor@gmx.de>
2013-10-31 02:43:32 +0100

    cleanup

--- christian/cleanup.sh
+++ christian/cleanup.sh
@@ -3,9 +3,9 @@
 rm -f *.out *.snm *.toc *.nav *.log *.aux *.vrb *.bbl *.blg *.orig *synctex.gz *.fdb_latexmk *.fls
 
 cd static
-rm -f *.out *.snm *.toc *.nav *.log *.aux *.vrb *.bbl *.blg *.orig
+rm -f *.out *.snm *.toc *.nav *.log *.aux *.vrb *.bbl *.blg *.orig *.fdb_latexmk
 cd ..
 
 cd tuts
-rm -f *.out *.snm *.toc *.nav *.log *.aux *.vrb *.bbl *.blg *.orig
+rm -f *.out *.snm *.toc *.nav *.log *.aux *.vrb *.bbl *.blg *.origi *.fdb_latexmk
 cd ..
