adcac97524731ce31cb2b7e474cd5e1e4433ff6c
Christian Jülg <gbi-tutor@gmx.de>
2013-10-31 02:36:09 +0100

    cleanup.sh

---
+++ christian/cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -f *.out *.snm *.toc *.nav *.log *.aux *.vrb *.bbl *.blg *.orig *synctex.gz *.fdb_latexmk *.fls
+
+cd static
+rm -f *.out *.snm *.toc *.nav *.log *.aux *.vrb *.bbl *.blg *.orig
+cd ..
+
+cd tuts
+rm -f *.out *.snm *.toc *.nav *.log *.aux *.vrb *.bbl *.blg *.orig
+cd ..
